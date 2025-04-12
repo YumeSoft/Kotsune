932cfdd741c2f38d847f810e4cb90bb6041a73e7
yunaswiss <yunaswiss@protonmail.ch>
2025-04-12 17:08:08 +0700

    New Types

--- app/src/main/java/me/thuanc177/kotsune/libs/mangaProvider/mangadex/MangaDexTypes.kt
+++ app/src/main/java/me/thuanc177/kotsune/libs/mangaProvider/mangadex/MangaDexTypes.kt
@@ -1,15 +1,19 @@
 package me.thuanc177.kotsune.libs.mangaProvider.mangadex
 
+import me.thuanc177.kotsune.viewmodel.SearchViewModel
+
 object MangaDexTypes {
     data class Manga(
         val id: String,
         val title: List<String>,
-        val coverImage: String?,
+        val poster: String?,
         val status: String,
         val description: String,
         val lastUpdated: String?,
         val lastChapter: Int?,
         val year: Int?,
-        val contentRating: String
+        val contentRating: String,
+        val tags: List<SearchViewModel.MangaTag> = emptyList(),
+        val rating: Float? = null
     )
 }
