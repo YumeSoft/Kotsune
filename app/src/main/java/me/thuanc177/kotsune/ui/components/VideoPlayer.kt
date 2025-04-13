0e25b04a2cd09c27ada4004638f8cccad20de855
AtelierMizumi <thuanc177@gmail.com>
2025-04-14 00:54:56 +0700

    Refactor VideoPlayer: update background shape and improve subtitle track selection logic
    Signed-off-by: AtelierMizumi <thuanc177@gmail.com>

--- app/src/main/java/me/thuanc177/kotsune/ui/components/VideoPlayer.kt
+++ app/src/main/java/me/thuanc177/kotsune/ui/components/VideoPlayer.kt
@@ -2,17 +2,50 @@ package me.thuanc177.kotsune.ui.components
 
 import android.content.Context
 import android.net.Uri
+import android.view.ViewGroup
 import android.view.ViewGroup.LayoutParams.MATCH_PARENT
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.*
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Cast
+import androidx.compose.material.icons.filled.Error
+import androidx.compose.material.icons.filled.Forward10
+import androidx.compose.material.icons.filled.Pause
+import androidx.compose.material.icons.filled.PlayArrow
+import androidx.compose.material.icons.filled.Replay10
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Divider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Slider
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -23,7 +56,11 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleEventObserver
-import androidx.media3.common.*
+import androidx.media3.common.C
+import androidx.media3.common.MediaItem
+import androidx.media3.common.MimeTypes
+import androidx.media3.common.PlaybackException
+import androidx.media3.common.Player
 import androidx.media3.common.util.UnstableApi
 import androidx.media3.datasource.DefaultDataSource
 import androidx.media3.exoplayer.ExoPlayer
@@ -31,13 +68,14 @@ import androidx.media3.exoplayer.hls.HlsMediaSource
 import androidx.media3.exoplayer.source.MediaSource
 import androidx.media3.exoplayer.source.MergingMediaSource
 import androidx.media3.exoplayer.source.ProgressiveMediaSource
+import androidx.media3.exoplayer.source.SingleSampleMediaSource
 import androidx.media3.exoplayer.trackselection.DefaultTrackSelector
 import androidx.media3.session.MediaSession
 import androidx.media3.ui.AspectRatioFrameLayout
 import androidx.media3.ui.PlayerView
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
-import java.util.*
+import me.thuanc177.kotsune.ui.screens.formatTime
 import kotlin.time.Duration.Companion.seconds
 
 @UnstableApi
@@ -216,7 +254,7 @@ fun VideoPlayer(
                         modifier = Modifier
                             .align(Alignment.Center)
                             .size(64.dp)
-                            .background(Color.Black.copy(alpha = 0.5f), shape = MaterialTheme.shapes.circular)
+                            .background(Color.Black.copy(alpha = 0.5f), shape = MaterialTheme.shapes.small)
                             .padding(8.dp)
                     ) {
                         Icon(
@@ -397,8 +435,7 @@ fun VideoPlayer(
                                     .clickable {
                                         player?.trackSelectionParameters = player?.trackSelectionParameters
                                             ?.buildUpon()
-                                            ?.setDisabledTextTrackSelectionFlags(C.SELECTION_FLAG_DEFAULT)
-                                            ?.build()
+                                            ?.build()!!
                                         showSubtitleSelector = false
                                     }
                                     .padding(vertical = 12.dp)
@@ -419,7 +456,7 @@ fun VideoPlayer(
                                                 player?.trackSelectionParameters = player?.trackSelectionParameters
                                                     ?.buildUpon()
                                                     ?.setPreferredTextLanguage(subtitleIndex.toString())
-                                                    ?.build()
+                                                    ?.build()!!
                                             }
                                             showSubtitleSelector = false
                                         }
