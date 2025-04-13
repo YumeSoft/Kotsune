ea63f2e01a80d33b5c01e0831d4dfe99f69178f5
AtelierMizumi <thuanc177@gmail.com>
2025-04-13 23:39:32 +0700

    Refactor AndroidManifest and Constants: update main activity name, improve context retrieval, and add new dependencies for Material3 and Media3
    Signed-off-by: AtelierMizumi <thuanc177@gmail.com>

--- app/src/main/java/me/thuanc177/kotsune/libs/Constants.kt
+++ app/src/main/java/me/thuanc177/kotsune/libs/Constants.kt
@@ -1,15 +1,13 @@
 package me.thuanc177.kotsune.libs
-
-import android.content.Context
-import me.thuanc177.kotsune.KotsuneApplication
-import java.io.File
+import me.thuanc177.kotsune.viewmodel.ViewModelContextProvider
 
 object Constants {
     const val ANILIST_ENDPOINT = "https://graphql.anilist.co"
     // Other ENDPOINT constants can be added here
 
     val APP_CACHE_DIR: String by lazy {
-        val context = KotsuneApplication.instance
+        val context = ViewModelContextProvider.context
+            ?: throw IllegalStateException("Application context is not available")
         // Primary choice is the app's cache directory
         val cacheDir = context.cacheDir
 
