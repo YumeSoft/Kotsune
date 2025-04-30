46756411d3b228ab65ccea88428123244477fdee
AtelierMizumi <thuanc177@gmail.com>
2025-04-30 20:39:50 +0700

    Add ReadManga feature with ViewModel and repository for chapter management
    Signed-off-by: AtelierMizumi <thuanc177@gmail.com>

---
+++ app/src/main/java/me/thuanc177/kotsune/repository/ChaptersRepository.kt
@@ -0,0 +1,23 @@
+package me.thuanc177.kotsune.repository
+
+import me.thuanc177.kotsune.viewmodel.ChapterModel
+
+/**
+ * Singleton repository to store chapter lists for manga
+ * Used to pass data between MangaDetailedScreen and ReadMangaScreen
+ */
+object ChaptersRepository {
+    private val chaptersMap = mutableMapOf<String, List<ChapterModel>>()
+
+    fun setChaptersForManga(mangaId: String, chapters: List<ChapterModel>) {
+        chaptersMap[mangaId] = chapters
+    }
+
+    fun getChaptersForManga(mangaId: String): List<ChapterModel> {
+        return chaptersMap[mangaId] ?: emptyList()
+    }
+
+    fun clearChapters() {
+        chaptersMap.clear()
+    }
+}
